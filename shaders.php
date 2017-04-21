47827c388e24ec8212929a81b2c09c6b25dc668b
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-04-21 17:50:41 +0200

    Add images and a video on the shaders page

--- shaders.php
+++ shaders.php
@@ -15,11 +15,23 @@
     <div class="container">
         <h2>Shaders, shader types and shader presets</h2>
         <p>A shader is a graphical filter that is applied to the game screen. You can use them to improve the aspect of old games and make them look smooth on recent monitors. They can also be used to replicate the old CRT monitors, or the screen of a gameboy.</p>
+
+        <p>Here are a few shader examples:</p>
+
+        <div class="row imagegrid">
+            <div class="col-md-4"><img src="images/shader-ex-1.png"></div>
+            <div class="col-md-4"><img src="images/shader-ex-2.png"></div>
+            <div class="col-md-4"><img src="images/shader-ex-3.png"></div>
+            <div class="col-md-4"><img src="images/shader-ex-4.png"></div>
+            <div class="col-md-4"><img src="images/shader-ex-5.png"></div>
+            <div class="col-md-4"><img src="images/shader-ex-6.png"></div>
+        </div>
+
         <p>Shaders are small programs, and they are written in specific programming languages. RetroArch supports 3 of these languages:</p>
         <ul>
-            <li>CG: the prefered format, when available on your platform</li>
-            <li>GLSL: these shaders are working on a wide range of platforms including phones and tablets</li>
-            <li>Slang: shaders compatible with the new Vulkan renderer</li>
+            <li><b>CG:</b> the prefered format, when available on your platform</li>
+            <li><b>GLSL:</b> these shaders are working on a wide range of platforms including phones and tablets</li>
+            <li><b>Slang:</b> shaders compatible with the new Vulkan renderer</li>
         </ul>
         <p>Depending on your platform and the way you have configured RetroArch, you need to use one of these shader types.</p>
         <p>RetroArch is also able to stack these shaders to create a combined effect. These complex effects are saved with a special extension:</p>
@@ -28,24 +40,27 @@
             <li>.glslp for GLSL</li>
             <li>.slangp for Slang</li>
         </ul>
-        <p>These presets have parameters. This means that you can tweak them to fit your needs.</p>
+
+        <p>The shader presets can also have parameters. This means that you can tweak them to fit your needs.</p>
 
         <h2>Downloading and upgrading shaders</h2>
         <p>You can download or upgrade the shader packs for the 3 types in <b>Main Menu->Online Updater</b>.</p>
 
         <h2>Loading a shader preset</h2>
-        <p>To enable a shader preset, you need to have a game running. You then trigger the menu and you should see a Shaders entry in the <b>Quick Menu</b>:</p>
 
         <div class="row">
-            <div class="col-md-6"><p><img src="images/load-shader-step-1.png"></p></div>
-            <div class="col-md-6"><p><img src="images/load-shader-step-2.png"></p></div>
-        </div>
-
-        <p>Go to <b>Load Shader Preset</b> and choose a preset file. In this example, I loaded <b>shaders_glsl/xbrz/4xbrz-linear.glslp</b>:</p>
-
-        <div class="row">
-            <div class="col-md-6"><p><img src="images/load-shader-step-3.png"></p></div>
-            <div class="col-md-6"><p><img src="images/load-shader-step-4.png"></p></div>
+            <div class="col-md-6">
+                <video width="100%" controls>
+                    <source src="videos/load-shader.webm" type="video/webm">
+                    Your browser does not support the video tag.
+                </video> 
+            </div>
+            <div class="col-md-6">
+                <p>To enable a shader preset, you need to have a game&nbsp;running.</p>
+                <p>You then trigger the menu and you should see a Shaders entry in the <b>Quick&nbsp;Menu</b>.</p>
+                <p>Go to <b>Load Shader Preset</b> and choose a preset&nbsp;file.</p>
+                <p>In this example, we have loaded <b>shaders_glsl/xbrz/4xbrz-linear.glslp</b>.</p>
+            </div>
         </div>
 
         <h2>Configuring a shader</h2>
