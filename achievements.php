d1275597f40756391f2adc2971f9d430692d33e7
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-04-23 17:06:11 +0200

    Achievements page

---
+++ achievements.php
@@ -0,0 +1,52 @@
+<div id="welcome">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 abstract">
+                <h1>Achievements</h1>
+                <p>Unlock achievements on your favorite classic&nbsp;games!</p>
+                <p>RetroArch integrates with the RetroAchievements service to provide a trophees unlocking mechanism similar to modern web&nbsp;consoles.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="doc">
+    <div class="container">
+        <div class="row imagegrid">
+            <div class="col-md-6">
+                <h2>How to setup achievements</h2>
+                <p>The first step is to register an accoun on <a href="http://retroachievements.org/">http://retroachievements.org/</a>.</p>
+                <p>Please note that this service is not maintained by RetroArch or the libretro team.</p>
+                <p>Then, you need to enable the functionnality and fill your retroachievements credentials in <b>Settings->Achievements</b>.</p>
+                <p>The <b>hardcore mode</b> prevents you to use emulation features like slow motion, fast forwarding, cheats and savestates.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="images/achievements-settings.png">
+            </div>
+        </div>
+        <div class="row imagegrid">
+            <div class="col-md-6">
+                <h2>Check your connection to the&nbsp;service</h2>
+                <p>You need an active internet connection.</p>
+                <p>In this example, we are using the game <b>Chrono Trigger (USA)</b> with the core <b>snes9x</b>.</p>
+                <p>Launch the game and trigger the <b>Quick Menu</b>.</p>
+                <p>Go to <b>Achievements</b> and you should see a list of the unlockable trophees for this game.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="images/achievements-list.png">
+            </div>
+        </div>
+        <div class="row imagegrid">
+            <div class="col-md-6">
+                <h2>Check your progress</h2>
+                <p>On the retroachievements website, you can login and access your account page.</p>
+                <p>You should be able to check your progress in the games and see which trophees you unlocked.</p>
+                <p>Trophees unlocked in hard mode are marked with a special color.</p>
+                <p>You can also check the progress of your friends and add comments on their trophees.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="images/achievements-progress.png">
+            </div>
+        </div>
+    </div>
+</div>
