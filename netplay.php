a3fca1c4eb496cb7a82352fb2ff6ec1f609ba16b
Twinaphex <libretro@gmail.com>
2017-04-27 11:38:55 +0200

    Update netplay.php

--- netplay.php
+++ netplay.php
@@ -4,7 +4,7 @@
             <div class="col-md-12 abstract">
                 <h1>Netplay</h1>
                 <p>Host or join a network gaming session.</p>
-                <p>Rediscover the joy of multiplayer games using RetroArch builtin netplay lobby.</p>
+                <p>Rediscover the joy of multiplayer games using RetroArch's builtin netplay lobby.</p>
                 <p>You can also use the spectator mode to watch others play.</p>
             </div>
         </div>
@@ -14,8 +14,8 @@
 <div class="doc">
     <div class="container">
         <h2>Presentation</h2>
-        <p>RetroArch enables you to play over the network to games that were supporting two or more players. It means that you can challenge players around the world on classic games like Mario Kart, Bomberman, Street Fighter, or team up on games like Streets of Rage or Sonic 3.</p>
-        <p>Participants will be connected in peer to peer to reduce network latency and ensure the best possible experience.</p>
+        <p>RetroArch enables you to play games online. You can challenge players around the world and compete on classic games like Mario Kart, Bomberman, Street Fighter, or team up on games like Streets of Rage or Sonic 3.</p>
+        <p>RetroArch relies on peer-to-peer networking to reduce network latency and ensure the best possible experience.</p>
 
         <div class="row imagegrid">
             <div class="col-sm-3"><img src="images/netplay-2p-1.png"></div>
@@ -27,11 +27,11 @@
         <h2>The lobby</h2>
         <div class="row imagegrid">
             <div class="col-sm-6">
-                <p>The most convenient way to try netplay is to use the <b>lobby</b> in the menu.</p>
+                <p>The most convenient way to try netplay is to use the <b>lobby</b> inside the menu.</p>
                 <p>You need an active internet connection and a router that supports UPnP port forwarding.</p>
-                <p>You will also need to own <b>the same ROM</b> as your partner and have it <b>scanned</b> and in your playlists. We recommand ROMs from the No-Intro sets.</p>
+                <p>You will also need to own <b>the same ROM</b> as your partner and have it <b>scanned</b> and in your playlists. We recommend ROMs from the No-Intro sets.</p>
                 <p>To join a netplay room in the lobby, just refresh the room list and press A on a room entry.</p>
-                <p>To host your own netplay session, press A on <b>Start netplay host</b> and a room will be created an announced publicly.</p>
+                <p>To host your own netplay session, press A on <b>Start netplay host</b> and a room will be created and announced publicly.</p>
                 <p>This lobby will also work for local rooms (on your home local network).</p>
             </div>
             <div class="col-sm-6">
@@ -47,7 +47,7 @@
             <div class="col-sm-6">
                 <p>Advanced users have the possibility to setup netplay manually.</p>
                 <p>The <b>Network Settings</b> enables you to tweak how the network code will behave when you are acting as <b>Server</b>.</p>
-                <p>If you don't need <b>NAT tarversal</b>, it can be disabled in the settings.</p>
+                <p>If you don't need <b>NAT traversal</b>, it can be disabled in the settings.</p>
                 <p>When you are acting as client, most of these settings will have no effect. The only things you will have to set will be the <b>Server IP</b>.</p>
             </div>
         </div>
@@ -65,6 +65,6 @@
         </div>
 
         <h2>Community</h2>
-        <p>Join our <a href="https://discord.gg/2G5EznA">Discord channels</a> to find netplayers and benefit of voice chat while playing.</p>
+        <p>Join our <a href="https://discord.gg/2G5EznA">Discord channels</a> to find netplayers and be able to use voice chat during gameplay.</p>
     </div>
 </div>
