6fafc63ea4c51b87e5ab63fa5ef370f1bc5f6153
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-05-01 10:03:03 +0200

    Indicators for the cores

--- about.php
+++ about.php
@@ -154,6 +154,7 @@
             <ol class="carousel-indicators">
                 <li data-target="#latestcores-carousel" data-slide-to="0" class="active"></li>
                 <li data-target="#latestcores-carousel" data-slide-to="1"></li>
+                <li data-target="#latestcores-carousel" data-slide-to="2"></li>
             </ol>
 
             <!-- Wrapper for slides -->
