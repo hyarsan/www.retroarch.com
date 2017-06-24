3f7a17fba9670fa612cc3c325ec4ae99380e5431
Rob Loach <robloach@gmail.com>
2017-06-24 11:40:30 -0400

    Add RetroArch Flatpak installation instructions

--- linux-instructions.php
+++ linux-instructions.php
@@ -17,9 +17,13 @@
         <p>Testing version:</p>
         <pre>sudo add-apt-repository ppa:libretro/testing && sudo apt-get update && sudo apt-get install retroarch</pre>
         <h2>Arch Linux</h2>
-        <p>Stable version</p>
+        <p>Stable version:</p>
         <pre>sudo pacman -Sy retroarch</pre>
         <p>Git version:</p>
         <pre>yaourt -Sy retroarch-git</pre>
+        <h2><a href="http://flatpak.org">Flatpak</a></h2>
+        <p>Stable version, hosted by <a href="https://flathub.org">Flathub</a>:</p>
+        <pre>flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo</pre>
+        <pre>flatpak install flathub org.libretro.RetroArch</pre>
     </div>
 </div>
