2230418315122d7bd4bbd6cc94f3fef7a1c6c2ac
Twinaphex <libretro@gmail.com>
2017-07-26 12:01:29 +0200

    Create donate.php

---
+++ donate.php
@@ -0,0 +1,18 @@
+ <?php $version = "1.6.0"; ?>
+
+<div id="welcome">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 abstract">
+                <h1>Ways to donate</h1>
+                <p>RetroArch/Libretro is a big project that consumes a lot of our resources and time. An impressive amount of spinoff products and derivatives (the vast majority not even affiliated with our project) all depend on its continuing development and online services.</p>
+                <p>To keep the show ongoing, there are various ways you can support the project.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="doc">
+    <div class="container">
+    </div>
+</div>
