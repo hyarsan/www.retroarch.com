4083a278eeec9b46f1f0cf38f081f0b7ac48ae12
Twinaphex <libretro@gmail.com>
2017-07-26 12:05:40 +0200

    Update donate.php

--- donate.php
+++ donate.php
@@ -14,6 +14,7 @@
 
 <div class="doc">
     <div class="container">
+     <p>
      <a href="https://www.patreon.com/bePatron?u=4061686" data-patreon-widget-type="become-patron-button">Become a Patron!</a><script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>
-    </div>
+     </p></div>
 </div>
