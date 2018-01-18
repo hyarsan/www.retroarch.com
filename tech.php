6f48a706c326fc5cee9aff317a8977daacdec460
Twinaphex <libretro@gmail.com>
2018-01-18 19:04:35 +0100

    Update tech.php

--- tech.php
+++ tech.php
@@ -11,15 +11,8 @@
 
 <div class="doc">
     <div class="container">
-                    <div class="row imagegrid" align="right">
-            <div class="col-md-6">
-                <video width="100%" controls>
-                    <source src="videos/latency.mp4" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video> 
-            </div>
         <h2>Latency</h2>
-        <p>RetroArch is truly in a league of its own when it comes to input responsiveness, and it keeps blowing away even us here at Libretro, to the degree that independent researchers start researching quite how good RetroArch can be and completely shattering several myths that have long been accepted as gospel in emulation circles:</p>
+        <p>RetroArch is truly in a league of its own when it comes to input responsiveness, and it keeps blowing away even us here at Libretro. Several independent researchers did their own research on RetroArch's latency and came away being quite blown away by the results, completely shattering several long-held myths that up until now had been accepted as gospel in emulation circles:</p>
         <p>
         <ul>
             <li>That emulation will always have an implicit 3 to 5 frames of input lag, and that therefore FPGA-based hardware will always hold a distinct advantage over software-based emulation.</li>
@@ -28,11 +21,19 @@
         </p>
         <p>RetroArch shatters these myths. It has been demonstratedy by independent researchers that a next-frame response time (≤16ms!) achievable with RetroArch! This means zero frames of input lag is achievable, indistinguishable from real hardware.</p>
    <p>
-       <blockquote>
-           "With Pitfall, I witnessed a response on the very next frame. In the video linked above, you can clearly see me hit the button near the end of one frame, and on the next, Harry jumps! Essentially no way to improve compared to original hardware. Pack it up. We’re done here"
-    </blockquote>
-    <p>So, whoever told you that input lag was a given with emulators and that you needed FPGA in order to avoid this latency, should get him/herself acquainted with RetroArch. Post-RetroArch, latency indistinguishable from real hardware is perfectly possible! 
+
+    <p>Whoever told you that input lag was a given with emulators and that you needed FPGA in order to avoid this latency, should get him/herself acquainted with RetroArch. Post-RetroArch, latency indistinguishable from real hardware is perfectly possible! 
 <p>Check out Brunnis' findings here on our forum and participate, don't just take our word for it! Link <a href="https://forums.libretro.com/t/an-input-lag-investigation/4407/534">here</a>.
     </p>
+                      <div class="row imagegrid" align="right">
+            <div class="col-md-6">
+                <video width="100%" controls>
+                    <source src="videos/latency.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video> 
+            </div>
+                                 <blockquote>
+           "With Pitfall, I witnessed a response on the very next frame. In the video linked above, you can clearly see me hit the button near the end of one frame, and on the next, Harry jumps! Essentially no way to improve compared to original hardware. Pack it up. We’re done here"
+    </blockquote>
     </div>
 </div>
